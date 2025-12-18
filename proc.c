44a0ce91061e73f0f5f283b80f252ba49e769df4
y0usef-2E <y0usef-2e@mailbox.org>
2025-12-18 17:22:00 +0100

    braindead rand function

--- proc.c
+++ proc.c
@@ -330,7 +330,6 @@ scheduler(void)
     // Enable interrupts on this processor.
     sti();
 
-    cprintf("generated %d\n", rand1());
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
@@ -340,6 +339,7 @@ scheduler(void)
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
+      cprintf("generated %d\n", rand1());
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
